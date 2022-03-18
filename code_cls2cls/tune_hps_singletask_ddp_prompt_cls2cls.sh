42cf91d926c4631726ecf8bf6ab7da4360548683
qcwthu <qcwthu@126.com>
2022-03-18 15:30:57 +0000

    cls2cls

---
+++ code_cls2cls/tune_hps_singletask_ddp_prompt_cls2cls.sh
@@ -0,0 +1,41 @@
+
+
+
+#TASKS=("superglue-cb" "dbpedia_14" "wiki_qa" "emo" "yelp_polarity" "ethos-religion" "amazon_polarity" "tab_fact" "anli" "ethos-race")
+
+TASKS=("anli")
+
+CHECKPOINT="None"
+#CHECKPOINT="models/upstream-multitask/last-model.pt"
+
+#size="base"
+size="large"
+
+IDENTIFIER="T5-"$size"-cls2cls"
+
+for TASK in ${TASKS[@]}
+do
+  echo "Task: $TASK, Checkpoint: $CHECKPOINT, Identifier: $IDENTIFIER"
+  python -m torch.distributed.launch --nproc_per_node 2 --master_port 29111 tune_hps_singletask_ddp_prompt_cls2cls.py \
+        --task_dir data/${TASK}/ \
+        --task_name ${TASK} \
+        --identifier $IDENTIFIER \
+        --checkpoint $CHECKPOINT \
+        --do_train \
+        --do_predict \
+        --learning_rate_list 5e-1 4e-1 3e-1 2e-1 \
+        --bsz_list 8 \
+        --predict_batch_size 16 \
+        --total_steps 3000 \
+        --eval_period 50 \
+        --warmup_steps 50 \
+        --num_train_epochs 1000.0 \
+        --gradient_accumulation_steps 1 \
+        --output_dir models/${IDENTIFIER}/singletask-${TASK} \
+        --cuda 0,1 \
+        --lm_adapted_path /export/share/sjoty/continual-learning/lm_adapted_model/torch_ckpt/$size/pytorch_model.bin \
+        --model google/t5-v1_1-$size \
+        --prompt_number 100
+      echo "++++++++++++++++++++++++++++++"
+      ps aux | grep tune_hps_singletask_ddp_prompt_cls2cls.py | awk '{print $2}' | xargs kill -9
+done
---
+++ code_cls2cls/tune_singletask_cls2cls.sh
@@ -0,0 +1,33 @@
+
+#TASKS=("race-high" "glue-qnli" "freebase_qa" "break-QDMR" "hatexplain" "circa" "wiki_split" "yelp_polarity")
+TASKS=("superglue-cb" "dbpedia_14" "wiki_qa" "emo" "yelp_polarity" "ethos-religion" "amazon_polarity" "tab_fact" "anli" "ethos-race")
+
+
+CHECKPOINT="None"
+size="large"
+IDENTIFIER="T5-"$size"-ft-cls2cls"
+
+for TASK in ${TASKS[@]}
+do
+  echo "Task: $TASK, Checkpoint: $CHECKPOINT, Identifier: $IDENTIFIER"
+  python -m torch.distributed.launch --nproc_per_node 2 --master_port 24567 tune_singletask_cls2cls.py \
+        --task_dir data/${TASK}/ \
+        --task_name ${TASK} \
+        --identifier $IDENTIFIER \
+        --checkpoint $CHECKPOINT \
+        --do_train \
+        --do_predict \
+        --learning_rate_list 5e-4 3e-4 2e-4 1e-4 \
+        --bsz_list 8 \
+        --predict_batch_size 32 \
+        --total_steps 1000 \
+        --eval_period 50 \
+        --warmup_steps 50 \
+        --num_train_epochs 300.0 \
+        --gradient_accumulation_steps 1 \
+        --output_dir models/${IDENTIFIER}/singletask-${TASK} \
+        --cuda 4,5 \
+        --model google/t5-v1_1-$size
+  echo "++++++++++++++++++++++++++++++"
+	ps aux | grep tune_singletask_cls2cls.py | awk '{print $2}' | xargs kill -9
+done
