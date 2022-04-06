dd84ff8c1e9fd5fb2f57b9239122b991f06f9588
qcw <qcwthu@126.com>
2022-04-07 00:15:54 +0800

    update

--- handleall.sh
+++ handleall.sh
@@ -1,33 +1,33 @@
 cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tasks/ -r
-cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tasks/ -r
+cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tasks/ -r
 cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/tasks/ -r
 cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/tasks/ -r
 cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/tasks/ -r
 cp task_file_nocls/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/tasks/ -r
 
 cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tasks/ -r
-cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tasks/ -r
+cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tasks/ -r
 cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/tasks/ -r
 cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/tasks/ -r
 cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/tasks/ -r
 cp newsettings/newtaskfile_fornli/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/tasks/ -r
 
 cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tasks/ -r
-cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tasks/ -r
+cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tasks/ -r
 cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/tasks/ -r
 cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/tasks/ -r
 cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/tasks/ -r
 cp newsettings/newtaskfile_forqa/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/tasks/ -r
 
 cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/dataloader/custom_tasks_splits/ -r
-cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/dataloader/custom_tasks_splits/ -r
 cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/dataloader/custom_tasks_splits/ -r
 cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/dataloader/custom_tasks_splits/ -r
 cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/dataloader/custom_tasks_splits/ -r
 cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/dataloader/custom_tasks_splits/ -r
 
 cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/data/ -r
-cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/data/ -r
+cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/data/ -r
 cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/data/ -r
 cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/data/ -r
 cp noclsdata/* /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/data/ -r
@@ -56,8 +56,8 @@ cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/cli_
 cp allscriptforQA/tune_hps_singletask_ddp_prompt_qa2qa.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/ -r
 cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tune_hps_singletask_ddp_prompt_cls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tune_hps_singletask_ddp_prompt_qa2qa.py -r
 
-cp allscriptforQA/tune_singletask_qa2qa.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/ -r
-cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tune_singletask_cls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tune_singletask_qa2qa.py -r
+cp allscriptforQA/tune_singletask_qa2qa.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/ -r
+cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tune_singletask_cls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tune_singletask_qa2qa.py -r
 
 cp allscriptforQA/singletask_from_meta_qa2qa.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/ -r
 cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/singletask_from_meta_cls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/singletask_from_meta_qa2qa.py -r
@@ -82,8 +82,8 @@ cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/sing
 cp allnewfolders/T5-large-qa2qa /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/models/ -r
 cp allnewfolders/T5-large-nonli2nli /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/models/ -r
 
-cp allnewfolders/T5-large-ft-qa2qa /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/models/ -r
-cp allnewfolders/T5-large-ft-nonli2nli /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/models/ -r
+cp allnewfolders/T5-large-ft-qa2qa /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/models/ -r
+cp allnewfolders/T5-large-ft-nonli2nli /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/models/ -r
 
 cp allnewfolders/T5-large-maml-qa2qa-3e-5-2-5000-5e-1 /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/models/ -r
 cp allnewfolders/T5-large-maml-noqa2qa-3e-5-2-5000-5e-1 /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/models/ -r
@@ -116,8 +116,8 @@ cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/cli_
 cp allscriptfornli/tune_hps_singletask_ddp_prompt_nonli2nli.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/ -r
 cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tune_hps_singletask_ddp_prompt_nocls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/tune_hps_singletask_ddp_prompt_nonli2nli.py -r
 
-cp allscriptfornli/tune_singletask_nonli2nli.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/ -r
-cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tune_singletask_nocls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_FT/tune_singletask_nonli2nli.py -r
+cp allscriptfornli/tune_singletask_nonli2nli.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/ -r
+cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tune_singletask_nocls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/tune_singletask_nonli2nli.py -r
 
 cp allscriptfornli/singletask_from_meta_nonli2nli.sh /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/ -r
 cp /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/singletask_from_meta_nocls2cls.py /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/singletask_from_meta_nonli2nli.py -r
