52a698a1c2f1b00c3f99113391716a94bcf18643
qcw <qcwthu@126.com>
2022-04-15 16:30:41 +0800

    shell file

---
+++ handleall_new_1.sh
@@ -0,0 +1,6 @@
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFitFT/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_multi/dataloader/custom_tasks_splits/ -r
+cp newsettings/*.json /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_reptile/dataloader/custom_tasks_splits/ -r
