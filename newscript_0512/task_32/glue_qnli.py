94ebefe1aa7f362f5e8cc25eba721b0cb9bd9407
qcw <qcwthu@126.com>
2022-05-12 22:13:00 +0800

    new py

---
+++ newscript_0512/task_32/glue_qnli.py
@@ -0,0 +1,43 @@
+import os
+import datasets
+import numpy as np
+
+from fewshot_gym_dataset import FewshotGymDataset, FewshotGymClassificationDataset
+
+class Glue_QNLI(FewshotGymClassificationDataset):
+    def __init__(self):
+        self.hf_identifier = "glue-qnli"
+
+        self.task_type = "classification"
+
+        # for classification tasks, specify the meaning of each label
+        self.label = {
+            0: "entailment",
+            1: "not_entailment",
+        }
+
+    def map_hf_dataset_to_list(self, hf_dataset, split_name):
+        lines = []
+        for datapoint in hf_dataset[split_name]:
+            # line[0]: input; line[1]: output
+            lines.append(("question: " + datapoint["question"] + " [SEP] sentence: " + datapoint["sentence"], self.label[datapoint["label"]]))
+        return lines
+
+    def get_train_test_lines(self, dataset):
+        train_lines = self.map_hf_dataset_to_list(dataset, "train")
+        test_lines = self.map_hf_dataset_to_list(dataset, "validation")
+        if len(test_lines) > 1000:
+            test_lines = test_lines[0:1000]
+        return train_lines, test_lines
+
+    def load_dataset(self):
+        return datasets.load_dataset('glue', 'qnli')
+
+def main():
+    dataset = Glue_QNLI()
+
+    for seed in [100, 13, 21, 42, 87]:
+        train, dev, test = dataset.generate_k_shot_data(k=32, seed=seed, path="../data_32/")
+
+if __name__ == "__main__":
+    main()
