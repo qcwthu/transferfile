a77da153e41f7d3e482cab9e14bd7f8e6e57c287
qcwthu <qcwthu@126.com>
2022-06-25 05:44:52 +0000

    base

---
+++ newfile_0622/alltouse_0622/cuda_23_para.sh
@@ -0,0 +1,34 @@
+####maml fomaml pt
+
+#cd CrossFit_fomaml/
+#echo "t5base para fomaml upstream"
+#bash cli_maml_ddp_prompt_fo_nopara2para_t5base.sh
+#rm -f /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/data/*/*.json
+#cd ..
+
+cd CrossFit_fomaml/
+echo "t5base para fomaml downstream"
+bash singletask_from_fomaml_nopara2para_t5base.sh
+rm -f /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_fomaml/data/*/*.json
+cd ..
+
+cd CrossFit/
+echo "t5base para pt downstream"
+bash tune_hps_singletask_ddp_prompt_nopara2para_t5base.sh
+rm -f /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit/data/*/*.json
+cd ..
+
+#cd CrossFit_maml/
+#echo "t5base para maml upstream"
+#bash cli_maml_ddp_prompt_nopara2para_t5base.sh
+#rm -f /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/data/*/*.json
+#cd ..
+
+cd CrossFit_maml/
+echo "t5base para maml downstream"
+bash singletask_from_meta_nopara2para_t5base.sh
+rm -f /export/share/sjoty/continual-learning/MetaPromptTuning/CrossFit_maml/data/*/*.json
+cd ..
+
+
+
